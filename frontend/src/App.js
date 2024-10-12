ff56285b8506833ca26aca41ba90e7118e5cc55c
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2024-10-12 10:32:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import MainNavbar from './MainNavbar';
-import ProfileNavbar from './ProfileNavbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
@@ -9,34 +7,22 @@ import Profile from './Profile';
 import MyGraphs from './MyGraphs';
 import './App.css';
 
-
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <div className="content">
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/login" element={< Login/>} />
-            <Route path="/register" element={< Register/>} />
-            <Route path="/profile" element={< Profile/>} />
-            <Route path="/MyGraphs" element={< MyGraphs/>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/MyGraphs" element={<MyGraphs />} />
           </Routes>
         </div>
       </div>
     </Router>
   );
-}
-
-function Navbar() {
-  const location = useLocation();
-
-  if (location.pathname.startsWith('/profile') || location.pathname.startsWith('/uploads') || location.pathname.startsWith('/my')) {
-    return <ProfileNavbar />;
-  }
-
-  return <MainNavbar />;
 }
 
 export default App;
